83653bd413d8a8602d63f1436bbfa2f63a697069
intelcore <becar_02@mail.ru>
2024-02-29 22:47:59 +0300

    Initial commit

---
+++ src/main/java/com/example/L20240219/L20240219Application.java
@@ -0,0 +1,13 @@
+package com.example.L20240219;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L20240219Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L20240219Application.class, args);
+	}
+
+}
